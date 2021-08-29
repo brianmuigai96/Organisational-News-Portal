bca007a630221ce33af50755f333f9db7e02b249
brianmuigai96 <brian.muigai@student.moringaschool.com>
2021-08-30 02:03:34 +0300

    using get method to get departments and users

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -28,6 +28,42 @@ public class App {
             int departmentId = Integer.parseInt(request.params("departmentId"));
             return gson.toJson(Department.findById(departmentId));
         });
+        get("/departments/:departmentId/users", "application/json", (request, response) -> {
+            int departmentId = Integer.parseInt(request.params("departmentId"));
+            return gson.toJson(Department.allDepartmentUsers(departmentId));
+        });
+
+
+        get("/departments/:departmentId/users/:userId/details", "application/json", (request, response) -> {
+            int employeeId = Integer.parseInt(request.queryParams("userId"));
+
+            User userToFind = User.findById(employeeId);
+
+            if (userToFind == null){
+                throw new ApiException(404, String.format("No user with id: \"%s\" exists", request.params("id")));
+            }
+
+            return gson.toJson(userToFind);
+        });
+
+
+        get("/users", "application/json", (request, response) -> {
+            return gson.toJson(User.all());
+        });
+
+
+        get("/users/:userId/details", "application/json", (request, response) -> {
+            int userId = Integer.parseInt(request.params("userId"));
+
+            User userToFind = User.findById(userId);
+
+            if (userToFind == null){
+                throw new ApiException(404, String.format("No user with id: \"%s\" exists", request.params("id")));
+            }
+
+            return gson.toJson(User.findById(userId));
+        });
+
 
 
     }
