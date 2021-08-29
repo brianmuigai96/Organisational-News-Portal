2589336ec73246559fd63d45adf1b9536a65e0e2
brianmuigai96 <brian.muigai@student.moringaschool.com>
2021-08-30 01:42:37 +0300

    declaring variables

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -11,5 +11,9 @@ import org.sql2o.Connection;
 import org.sql2o.Sql2oException;
 
 public class Department {
+    private String name;
+    private String description;
+    private int employee_count;
+    private int id;
 
 }
