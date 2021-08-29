26bd1570b5561e1d4a20f7c9d2683d49db07d74e
brianmuigai96 <brian.muigai@student.moringaschool.com>
2021-08-30 01:52:10 +0300

    adding setters and getters

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -7,5 +7,42 @@ public class News {
     private String description;
     private String type;
     private int id;
+    public News(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 
 }
