94146855287fcd62cd39ab1db01ba168da136fcb
brianmuigai96 <brian.muigai@student.moringaschool.com>
2021-08-30 01:47:27 +0300

    overriding object

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -54,7 +54,42 @@ public class Department {
     public int reduceEmployeeCount(){
         return this.employee_count--;
     }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Department)) return false;
+        Department that = (Department) o;
+        return getEmployee_count() == that.getEmployee_count() &&
+                getId() == that.getId() &&
+                Objects.equals(getName(), that.getName()) &&
+                Objects.equals(getDescription(), that.getDescription());
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getDescription(), getEmployee_count(), getId());
+    }
+
+    public static void add(Department department) {
+        try(Connection conn = DB.sql2o.open()){
+            String sql = "INSERT INTO departments (name, description, employee_count) VALUES (:name, :description, :employeeCount)";
+            int id = (int) conn.createQuery(sql, true)
+                    .bind(department)
+                    .addParameter("employeeCount", department.getEmployee_count())
+                    .executeUpdate()
+                    .getKey();
+            department.setId(id);
+        } catch (Sql2oException ex){
+            System.out.println("Unable to add department" + ex);
+        }
+    }
+
+    public static List<Department> all(){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "SELECT * FROM departments";
+            return con.createQuery(sql).executeAndFetch(Department.class);
+        }
+    }
 
 
 }
