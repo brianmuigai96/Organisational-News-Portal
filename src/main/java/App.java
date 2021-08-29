452a528aa5f68f3cc4674561deeede39ea6727f1
brianmuigai96 <brian.muigai@student.moringaschool.com>
2021-08-30 02:01:11 +0300

    returning departments with id

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,4 +8,26 @@ import com.google.gson.Gson;
 import org.sql2o.Sql2o;
 
 public class App {
-}
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+        Gson gson = new Gson();
+
+        get("/", "application/json", (request, response) -> {
+            return gson.toJson(Department.all());
+        });
+
+
+        get("/departments/:departmentId", "application/json", (request, response) -> {
+            int departmentId = Integer.parseInt(request.params("departmentId"));
+            return gson.toJson(Department.findById(departmentId));
+        });
+
+
+    }
