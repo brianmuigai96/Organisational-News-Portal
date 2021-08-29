acdbc98cbe3f88fdf04db30ea52b34cbdd964097
brianmuigai96 <brian.muigai@student.moringaschool.com>
2021-08-30 01:56:21 +0300

    generating the getters and setters

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -14,4 +14,43 @@ public class User {
     private String position;
     private String role;
     private String department;
+    public User(String name, String position, String role, String department) {
+        this.name = name;
+        this.position = position;
+        this.role = role;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
