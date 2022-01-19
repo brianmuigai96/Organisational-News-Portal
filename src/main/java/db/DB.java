83c38c99eb0b6eae1f8f1fa43fcb7162640c3b4b
brianmuigai96 <brian.muigai@student.moringaschool.com>
2022-01-19 17:48:09 +0300

    adding changes

--- src/main/java/db/DB.java
+++ src/main/java/db/DB.java
@@ -2,6 +2,6 @@ package db;
 import org.sql2o.*;
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-225-187-177.compute-1.amazonaws.com:5432/dfe1jsmaprbn2e", "yyaicgxoththsd", "681c422ccafcf464a70a233d17a123770d44020d04b88e7589a2f8d9b57f2c19");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-225-187-177.compute-1.amazonaws.com:5432/dfe1jsmaprbn2e?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory", "yyaicgxoththsd", "681c422ccafcf464a70a233d17a123770d44020d04b88e7589a2f8d9b57f2c19");
 //        public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/news_portal", "brian", "1234");
 }
--- src/test/java/models/DatabaseRule.java
+++ src/test/java/models/DatabaseRule.java
@@ -6,7 +6,7 @@ import org.junit.rules.ExternalResource;
 public class DatabaseRule extends ExternalResource{
     @Override
     public void before(){
-        DB.sql2o = new Sql2o("jdbc:postgresql://ec2-54-225-187-177.compute-1.amazonaws.com:5432/dfe1jsmaprbn2e", "yyaicgxoththsd", "681c422ccafcf464a70a233d17a123770d44020d04b88e7589a2f8d9b57f2c19");}
+        DB.sql2o = new Sql2o("jdbc:postgresql://ec2-54-225-187-177.compute-1.amazonaws.com:5432/dfe1jsmaprbn2e?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory", "yyaicgxoththsd", "681c422ccafcf464a70a233d17a123770d44020d04b88e7589a2f8d9b57f2c19");}
 
 
     @Override
